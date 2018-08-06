0139b5a4eed4dc638ce57a4d04e469d488ce5329
KevinZc <759441212@qq.com>
2018-08-06 22:46:51 +0800

    郑超

--- src/pages/orders/order.js
+++ src/pages/orders/order.js
@@ -1,8 +1,16 @@
 const app = getApp()
 Page({
   data: {
-    orders:[
-      {orderTime:'2018-8-5 16:57:15',storeName:'CoCo都可（成都市高新店）',getOrderTime:'2018-8-5 16:59:38',orderType:'预约订单', state:'已退款',orderNum:'C088',price:'￥10.0'}
+    orders: [
+      {
+        orderTime: '2018-8-5 16:57:15',
+        storeName: 'CoCo都可（成都市高新店）',
+        getOrderTime: '2018-8-5 16:59:38', 
+        orderType: '预约订单', 
+        state: '已退款', 
+        orderNum: 'C088', 
+        price: '￥10.0'
+      }
     ]
   },
   //事件处理函数
