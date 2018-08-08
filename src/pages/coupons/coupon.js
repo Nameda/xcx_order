fe9f8459f01c11ce75f4e7039d7539e69555b1fa
KevinZc <759441212@qq.com>
2018-08-08 23:14:34 +0800

    郑超

--- src/pages/coupons/coupon.js
+++ src/pages/coupons/coupon.js
@@ -6,7 +6,8 @@ Page({
      */
     data: {
         coupons:[
-            {name:'赵丽颖红包',price:'￥50.0',expireTime:'2019-03-26',remark:'满100.0可用'}
+            {name:'婷姐红包',price:'￥50.0',expireTime:'2019-03-26',remark:'满100.0可用'},
+            {name:'超哥红包',price:'￥100.0',expireTime:'2019-03-26',remark:'无门槛'}
         ]
     }
 });
--- src/pages/orders/order.js
+++ src/pages/orders/order.js
@@ -10,6 +10,24 @@ Page({
         state: '已退款', 
         orderNum: 'C088', 
         price: '￥10.0'
+      },
+      {
+        orderTime: '2018-8-7 16:57:15',
+        storeName: 'CoCo都可（成都市高新店）',
+        getOrderTime: '2018-8-5 16:59:38', 
+        orderType: '预约订单', 
+        state: '已确定', 
+        orderNum: 'C089', 
+        price: '￥15.0'
+      },
+      {
+        orderTime: '2018-8-6 16:57:15',
+        storeName: 'CoCo都可（成都市高新店）',
+        getOrderTime: '2018-8-5 16:59:38', 
+        orderType: '预约订单', 
+        state: '已退款', 
+        orderNum: 'C099', 
+        price: '￥17.0'
       }
     ]
   },
@@ -19,5 +37,10 @@ Page({
   },
   onLoad() {
     // getCourseList()
+  },
+  goDetail(){
+    wx.navigateTo({
+      url: "/pages/orderDetail/orderDetail"
+    });
   }
 })
