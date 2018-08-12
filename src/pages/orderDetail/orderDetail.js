b0b705bba71360dddf076edc37b0ba85e516da66
KevinZc <759441212@qq.com>
2018-08-12 22:02:26 +0800

    郑超

--- src/pages/orderDetail/orderDetail.js
+++ src/pages/orderDetail/orderDetail.js
@@ -5,5 +5,18 @@ Page({
      * 页面的初始数据
      */
     data: {
+        orderDetail:{
+            orderType:'新订单1',
+            getOrderNo:'C088',
+            expectGetTime:'2018-08-08 20:30:00',
+            goodsNameSize:'珍珠奶茶/中杯',
+            goodsPrice:'￥10.0',
+            goodsNum:'x1',
+            goodsTotalPrice:'￥10.0',
+            orderTotalPrice:'￥10.0',
+            orderNo:'WD10731193321240577',
+            orderTime:'2018-07-31 19:32:02',
+            storeAddress:'四川省成都市青羊区日月大道一段978号1栋附4015号'
+        }
     }
 });
